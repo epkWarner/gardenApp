273d0642391e2340111deba8a881a1e0b0d95d56
epkWarner <evan.k.warner@gmail.com>
2018-10-24 16:08:34 -0400

    added empty component for all routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SplashComponent } from './splash/splash.component'
+import { AboutComponent } from './about/about.component'
+import { AdminComponent } from './admin/admin.component'
+import { LoginComponent } from './login/login.component'
+import { ShopComponent } from './shop/shop.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '/home', component: SplashComponent },
-  { path: '/about', component: AboutComponent },
-  { path: '/shop', component: ShopComponent },
-  { path: '/admin', component: AdminComponent }
+  { path: 'home', component: SplashComponent },
+  // { path: '/about', component: AboutComponent },
+  // { path: '/shop', component: ShopComponent },
+  // { path: '/admin', component: AdminComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { SplashComponent } from './splash/splash.component';
+import { AboutComponent } from './about/about.component';
+import { ShopComponent } from './shop/shop.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    SplashComponent
+    SplashComponent,
+    AboutComponent,
+    ShopComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
