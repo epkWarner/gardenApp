8948d15fba41b999f4d82f59c5d44b4d89d4ece0
epkWarner <evan.k.warner@gmail.com>
2018-10-24 15:43:47 -0400

    added routing to app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '/home', component: SplashComponent },
+  { path: '/about', component: AboutComponent },
+  { path: '/shop', component: ShopComponent },
+  { path: '/admin', component: AdminComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
